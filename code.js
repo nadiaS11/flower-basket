ba0774f8fc9566be154dcd763c75232d9de15d52
nadiaS11 <nadiasultana.ns11@gmail.com>
2023-08-19 03:01:13 +0600

    getdiscount edited

--- code.js
+++ code.js
@@ -1,40 +1,43 @@
-let total =0;
-let grandTotal =0;
+// function getDiscount(e){
+//     const discountInput= e.parentNode.childNodes[3]
+//     const discount= discountInput.value
 
-function handleClick(e){
-    const cartItems= document.getElementById("selected-items")
-    const totalAmount= document.getElementById("total-amount")
-    const grandTotalAmount= document.getElementById("grand-amount")
-    const discountInput= document.getElementById("discount-input")
-    
-    const discount=parseFloat(discountInput.value)
-    
-    const getPrice = parseInt(e.parentNode.childNodes[5].innerText.split(" ")[1])
-    
-    total= parseInt(total)+parseInt(getPrice)
-    
-    totalAmount.innerText= total
-    const getName = e.parentNode.childNodes[3].innerText;
+//     const parsedDiscount = parseFloat(discount); // Use parseFloat for decimal numbers
+//     console.log(parsedDiscount);
+// }
 
-    grandTotal= total
-    grandTotalAmount.innerText=grandTotal
-    
-    
-    // Check if the flower name already exists in the cart
-    let nameExists = false;
-    for (const item of cartItems.childNodes) {
-        if (item.innerText === getName) {
-            nameExists = true;
-            break;
-        }
+let total = 0;
+let grandTotal = 0;
+
+function handleClick(e) {
+  const cartItems = document.getElementById("selected-items");
+  const totalAmount = document.getElementById("total-amount");
+  const grandTotalAmount = document.getElementById("grand-amount");
+
+  const getPrice = parseInt(e.parentNode.childNodes[5].innerText.split(" ")[1]);
+
+  total = parseInt(total) + parseInt(getPrice);
+
+  totalAmount.innerText = total;
+  const getName = e.parentNode.childNodes[3].innerText;
+
+  grandTotal = total;
+  grandTotalAmount.innerText = grandTotal;
+
+  // Check if the flower name already exists in the cart
+  let nameExists = false;
+  for (const item of cartItems.childNodes) {
+    if (item.innerText === getName) {
+      nameExists = true;
+      break;
     }
+  }
 
-    // If the name doesn't exist in the cart, add it
-    if (!nameExists) {
-        const p = document.createElement('p');
-        p.innerText = getName;
-        p.style.fontWeight = '500';
-        cartItems.appendChild(p);
-    }
-console.log(discount)
+  // If the name doesn't exist in the cart, add it
+  if (!nameExists) {
+    const p = document.createElement("p");
+    p.innerText = getName;
+    p.style.fontWeight = "500";
+    cartItems.appendChild(p);
+  }
 }
